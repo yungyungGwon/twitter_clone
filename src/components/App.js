8c0ee31577e15e83b9247835b89fe1c7ba6a3654
yungyungGwon <gywn7724@gmail.com>
2021-11-24 16:59:41 +0900

    2021.11.23 (firebase setting, error resolve)

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import AppRouter from './Router';
+import AppRouter from 'components/Router';
 import {authService} from '../fbase';
-console.log(authService)
+
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  console.log(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <>
       <AppRouter isLoggedIn={isLoggedIn}/>
       <footer> &copy;{new Date().getFullYear()} Nwitter</footer>
-   </>
+    </>
   );
 }
 
